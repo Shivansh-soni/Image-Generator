9830a22da9a82334e34c379f8c910dbe95efc325
Shivansh-soni <shivanshsoni.indore@gmail.com>
2022-12-25 00:35:41 +0530

    25/12 12:35AM

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -36,7 +36,7 @@ function Hero() {
                 e.preventDefault();
 
                 const response = await fetch(
-                  "http://localhost:5000/openai/generateimage",
+                  "https://cute-cyan-adder.cyclic.app/openai/generateimage",
                   {
                     method: "POST",
                     headers: {
