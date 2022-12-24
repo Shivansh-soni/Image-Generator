f23f5667ddb74a95acfe5c492b65090d21daf4cc
Shivansh-soni <shivanshsoni.indore@gmail.com>
2022-12-24 20:44:29 +0530

    Frontend

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 function Hero() {
   const [text, setText] = React.useState("");
+  const [img, setImg] = React.useState("");
+
   return (
     <div
       style={{
@@ -12,9 +14,9 @@ function Hero() {
       }}
     >
       {" "}
-      <div className="card lg:card-side bg-base-100 shadow-xl">
+      <div className="card lg:card-side w-full bg-base-100 shadow-xl">
         <figure>
-          <img src="https://placeimg.com/400/400/arch" alt="Album" />
+          <img src={img} alt="Album" />
         </figure>
         <div className="card-body flex flex-col gap-20 justify-center">
           <h2 className="card-title text-2xl">AI based Image Generator</h2>
@@ -28,7 +30,37 @@ function Hero() {
             className="input input-bordered w-full max-w-xs"
           />
           <div className="card-actions items-center justify-center">
-            <button className="btn btn-primary ">Listen</button>
+            <button
+              className="btn btn-primary "
+              onClick={async (e) => {
+                e.preventDefault();
+
+                const response = await fetch(
+                  "http://cute-cyan-adder.cyclic.app/openai/generateimage",
+                  {
+                    method: "POST",
+                    headers: {
+                      "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                      prompt: text,
+                      size: "medium",
+                    }),
+                  }
+                );
+
+                const data = await response.json();
+                console.log(data);
+                if (data.success == true) {
+                  setImg(data.data);
+                } else {
+                  alert("Error! Please try again");
+                  setText("");
+                }
+              }}
+            >
+              Listen
+            </button>
           </div>
           <p></p>
         </div>
