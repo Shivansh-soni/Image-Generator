b80fc250fe6f973194c95f89005cbf2d4fd6efb0
Shivansh-soni <shivanshsoni.indore@gmail.com>
2022-12-25 12:39:07 +0530

    FInal

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { BallTriangle } from "react-loader-spinner";
 
 function Hero() {
   const [text, setText] = React.useState("");
-  const [img, setImg] = React.useState("");
+  const [img, setImg] = React.useState(
+    "https://img.freepik.com/free-photo/ai-nuclear-energy-industry-innovation-smart-grid-disruptive-technology_53876-143121.jpg?w=740&t=st=1671949871~exp=1671950471~hmac=7aace702d6f527d1f17e00c87fe60d5409cdba6544c57d6354b421cd7e845bf3"
+  );
+  const [loader, setLoader] = React.useState(false);
+  const [visible, setVisible] = React.useState("none");
 
   return (
     <div
@@ -13,9 +18,8 @@ function Hero() {
         transform: "translate(-50%, -50%)",
       }}
     >
-      {" "}
-      <div className="card lg:card-side w-full bg-base-100 shadow-xl">
-        <figure>
+      <div className="card lg:w-full lg:card-side bg-base-100 shadow-xl">
+        <figure className="">
           <img src={img} alt="Album" />
         </figure>
         <div className="card-body flex flex-col gap-20 justify-center">
@@ -34,7 +38,8 @@ function Hero() {
               className="btn btn-primary "
               onClick={async (e) => {
                 e.preventDefault();
-
+                setVisible("flex");
+                setLoader(true);
                 const response = await fetch(
                   "https://cute-cyan-adder.cyclic.app/openai/generateimage",
                   {
@@ -50,10 +55,15 @@ function Hero() {
                 );
 
                 const data = await response.json();
+
                 console.log(data);
                 // eslint-disable-next-line
                 if (data.success == true) {
                   setImg(data.data);
+                  setTimeout(() => {
+                    setVisible("none");
+                    setLoader(false);
+                  }, 5000);
                 } else {
                   alert("Error! Please try again");
                   setText("");
@@ -64,6 +74,31 @@ function Hero() {
             </button>
           </div>
           <p></p>
+          <div
+            style={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              background: "rgb(0,0,0 , 0.8)",
+              display: `${visible}`,
+              justifyContent: "center",
+              alignItems: "center",
+              width: "100vw",
+              height: "100vh",
+            }}
+          >
+            <BallTriangle
+              height={100}
+              width={100}
+              radius={5}
+              color="#4fa94d"
+              ariaLabel="ball-triangle-loading"
+              wrapperClass={{}}
+              wrapperStyle=""
+              visible={loader}
+            />
+          </div>
         </div>
       </div>
     </div>
